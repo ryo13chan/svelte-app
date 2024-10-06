bba193bf309d0448449dc56f412707598f6825e3
Narita Ryosuke <ryo13chan@gmail.com>
2024-10-06 16:01:52 +0900

    アイコン削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 import { Button } from '$lib/components/ui/button'
-import Check from 'svelte-material-icons/Check.svelte'
 </script>
 
-<Check size="24" />
 <Button>Click me</Button>
 <h1 class="text-3xl font-bold underline">
   Hello world!
